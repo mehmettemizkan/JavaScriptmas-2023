a5bbbd91d367d07c20ccf433c94d38bfeed8cae2
mehmettemizkan <mehmettemizkan88@gmail.com>
2023-12-18 16:45:22 +0300

    Day 18 Challenge Solved.

---
+++ Day 18 AI Alt Text Generator/index.js
@@ -0,0 +1,59 @@
+import { HfInference } from '@huggingface/inference'
+const hf = new HfInference()
+import { blobToBase64 } from '/utils'
+
+const dialogModal = document.getElementById('dialog-modal')
+dialogModal.show()
+
+document.addEventListener('submit', function (e) {
+        e.preventDefault()
+        const imageDescription = document.getElementById('user-input').value
+        dialogModal.close()
+        generateImage(imageDescription)
+
+
+})
+
+async function generateImage(imageToGenerate) {
+        const response = await hf.textToImage({
+                inputs: imageToGenerate,
+                model: "stabilityai/stable-diffusion-2",
+        })
+        const imageUrl = await blobToBase64(response)
+
+        generateAltText(imageUrl)
+}
+
+async function generateAltText(imageUrl) {
+        const altText = await hf.imageToText({
+                data: await (await fetch(imageUrl)).blob(),
+                model: "Salesforce/blip-image-captioning-base",
+        });
+
+        renderImage(imageUrl, altText)
+}
+
+const renderImage = (imageUrl, altText) => {
+
+        const imageContainer = document.getElementById('image-container')
+        imageContainer.innerHTML = ''
+        const image = document.createElement('img')
+        image.src = imageUrl
+        image.alt = altText.generated_text
+        imageContainer.appendChild(image)
+
+        const imageHeader = document.createElement('p')
+        imageHeader.innerText = capitalizeWords(altText.generated_text)
+        imageContainer.appendChild(imageHeader)
+}
+
+const capitalizeWords = (str) => {
+        // Split the string by spaces
+        const words = str.split(" ");
+
+        // Capitalize the first letter of each word
+        const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+
+        // Join the capitalized words and return the result
+        return capitalizedWords.join(" ");
+};
