9ccdf44289deb0f756f55c75a57226b78dcc129f
mehmettemizkan <mehmettemizkan88@gmail.com>
2023-12-20 10:16:52 +0300

    Initial files for day 20 upload.

---
+++ Day 20 Save Santa/index.js
@@ -0,0 +1,24 @@
+const dangerArray = [
+        ["🎅", "👺"],
+        [
+                ["🎅", "🦁"],
+                ["👹", "🎅"]
+        ],
+        [
+                [
+                        ["🎅", "🐻"],
+                        ["🧌", "🎅"]
+                ],
+                [
+                        ["🐯", "🎅"],
+                        ["🎅", "😈"]
+                ]
+        ]
+];
+
+function saveSanta(arr) {
+        // Your code here
+}
+
+// Check the returned results from saveSanta()
+console.log(saveSanta(dangerArray))
